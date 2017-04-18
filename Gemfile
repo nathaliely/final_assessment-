a01357cdcda3bc7522d4cc477cf07bc596b1f853
nathalie <nathlyv@gmail.com>
2017-04-18 14:25:06 +0800

    first commit

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
@@ -54,3 +55,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # authentification
 gem 'bcrypt', '~> 3.1.7'
+
+#Omniauth and Omniauth-Facebook gems
+gem 'omniauth'
+gem 'omniauth-facebook', '1.4.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users
+  resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
