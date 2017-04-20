1fade4dbadb388daba7f9c4be61a572461bc8cd1
nathalie <nathlyv@gmail.com>
2017-04-20 23:06:17 +0800

    rspec

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+
+
 RSpec.describe User, type: :model do
 
   # testing active record associations using shoulda-matchers
@@ -16,4 +18,19 @@ RSpec.describe User, type: :model do
     it { is_expected.to allow_value("good@email.com").for(:email) }
     # it { is_expected.not_to allow_value("bad@email").for(:email) }
   end
+
+
+     it "should have username and email and password_digest" do
+        should have_db_column(:name).of_type(:string)
+        should have_db_column(:email).of_type(:string)
+        should have_db_column(:password_digest).of_type(:string)
+     end
+
+     describe "required validation for creating new users" do
+        it {should have_secure_password}
+        it {should validate_uniqueness_of(:email)}
+        it {should validate_presence_of(:email)}
+    end
+
+
 end
